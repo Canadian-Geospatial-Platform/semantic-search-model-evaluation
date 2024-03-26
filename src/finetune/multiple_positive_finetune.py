7ef023dc4a332afc67b3bfcdb2e783f07bf02269
Sajad <sajjadoncall@gmail.com>
2024-03-26 12:48:31 -0400

    feat: add logging to the mpf

--- src/finetune/multiple_positive_finetune.py
+++ src/finetune/multiple_positive_finetune.py
@@ -1,6 +1,23 @@
 import pandas as pd
 from nltk.tokenize import sent_tokenize
 from finetune import preprocess_records_into_text
+import nltk
+
+from sentence_transformers import SentenceTransformer, InputExample, losses
+from torch.utils.data import DataLoader
+import logging
+
+nltk.download('punkt')
+
+# Configure logging to file and console
+logging.basicConfig(level=logging.INFO, 
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    handlers=[
+                        logging.FileHandler("training_log_MPR.log"),
+                        logging.StreamHandler()
+                    ])
+logger = logging.getLogger(__name__)
+
 def row_to_sentence_pairs(df, text_column):
     pairs = []
     for text in df[text_column]:
@@ -24,27 +41,28 @@ def dataframe_to_sentence_pairs(df, text_column):
         pairs.append({"set": [part1, part2]})
     return pairs
 
+def callback(score, epoch, steps):
+    logger.info(f"Epoch: {epoch}, Steps: {steps}, Loss: {score}")
+    
 # Example usage
-
-df = pd.read_parquet('records.parquet')
-df['text'] = preprocess_records_into_text(df)
-
-sentence_pairs = dataframe_to_sentence_pairs(df, 'text')
-
-from sentence_transformers import SentenceTransformer, InputExample, losses
-from torch.utils.data import DataLoader
-
-# Load the pre-trained model
-model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
-
-# Convert the sentence pairs to InputExample format
-examples = [InputExample(texts=pair["set"]) for pair in sentence_pairs]
-
-# DataLoader
-train_dataloader = DataLoader(examples, batch_size=16, shuffle=True)
-
-# MultipleNegativesRankingLoss
-train_loss = losses.MultipleNegativesRankingLoss(model)
-
-# Training
-model.fit(train_objectives=[(train_dataloader, train_loss)], epochs=1, warmup_steps=100)
+if __name__ == '__main__':
+    df = pd.read_parquet('records.parquet')
+    df['text'] = preprocess_records_into_text(df)
+    
+    sentence_pairs = dataframe_to_sentence_pairs(df, 'text')
+    
+    
+    # Load the pre-trained model
+    model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
+    
+    # Convert the sentence pairs to InputExample format
+    examples = [InputExample(texts=pair["set"]) for pair in sentence_pairs[:128]]
+    
+    # DataLoader
+    train_dataloader = DataLoader(examples, batch_size=32, shuffle=True)
+    
+    # MultipleNegativesRankingLoss
+    train_loss = losses.MultipleNegativesRankingLoss(model)
+    
+    # Training
+    model.fit(train_objectives=[(train_dataloader, train_loss)], epochs=4, warmup_steps=100, output_path='model_output_MNR', callback=callback)
