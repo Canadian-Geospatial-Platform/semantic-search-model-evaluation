7bb93884d1d18179285d952adeadcbd7628e922e
svetlana-esina <svetlana.esina@nrcan-rncan.gc.ca>
2026-06-23 08:17:03 -0400

    Fixed filepath typo in collect results

--- src/collect_results.py
+++ src/collect_results.py
@@ -67,7 +67,7 @@ def main(config_path, save_path):
             continue
         
         df = run_generate_model_summary(model_config['model'], model_config['training_configs'])
-        if df:
+        if df is not None:
             all_results.append(df)
 
 
@@ -78,7 +78,7 @@ def main(config_path, save_path):
     print("Done")
 
 if __name__ == "__main__":
-    config_path = "/space/partner/nrcan/geobase/work/oatt/dev/semanticsearch/code/finetune/configs/finetune-via-trainer-best-eval-se.json"
+    config_path = "/space/partner/nrcan/geobase/work/oatt/dev/semanticsearch/code/src/finetune/configs/finetune-via-trainer-best-eval-se.json"
     save_path = "/space/partner/nrcan/geobase/work/oatt/dev/semanticsearch/results/finetune_via_trainer/summary_on_test.csv"
 
     main(config_path, save_path)
